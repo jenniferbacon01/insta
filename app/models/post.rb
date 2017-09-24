76fe6c250c22b4934cdebe390e3613afb279af58
Jenny Bacon <jenniferbacon01@gmail.com>
2017-09-24 21:53:31 +0100

    user email on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 # class Post < ApplicationRecord
 class Post < ActiveRecord::Base
 
+  validates :user_id, presence: true
+
+  belongs_to :user
+
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
