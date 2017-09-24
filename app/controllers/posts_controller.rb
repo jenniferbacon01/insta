b2bc364689d472eccda326d29d469116f330e23c
Jenny Bacon <jenniferbacon01@gmail.com>
2017-09-24 17:47:23 +0100

    editing and deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -13,9 +14,32 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
